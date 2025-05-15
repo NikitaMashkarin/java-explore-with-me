f015055ceea9da92a8443c8b0da71bbc2f11cf10
NikitaMashkarin <mashkarinnikita@yandex.ru>
2025-05-15 21:28:36 +0300

    Исправить checkstyle

--- stat-service/stat-client/src/main/java/ru/practicum/StatClient.java
+++ stat-service/stat-client/src/main/java/ru/practicum/StatClient.java
@@ -1,11 +1,9 @@
 package ru.practicum;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
